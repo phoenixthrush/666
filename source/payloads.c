e7589f4c4c6a3c3b98d1280e8d0a3674c665023b
Phoenixthrush UwU <mr.cetto@phoenixthrush.com>
2023-10-12 11:40:20 +0200

    create basic folder structure

--- source/payloads.c
+++
@@ -1,8 +0,0 @@
-#include "payloads.h"
-
-#include <stdio.h>
-
-int payloads(int argc, char *argv[]) {
-    printf("Hello, world from payloads.c!\n");
-    return 0;
-}
--- source/prepare.c
+++
@@ -1,8 +0,0 @@
-#include "prepare.h"
-
-#include <stdio.h>
-
-int prepare(int argc, char *argv[]) {
-    printf("Hello, world from prepare.c!\n");
-    return 0;
-}
