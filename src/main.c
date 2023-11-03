df37a3bd2b07b33d52d3f812af6ca824325fdd43
Phoenixthrush UwU <mr.cetto@phoenixthrush.com>
2023-11-03 21:27:19 +0100

    add platform check

--- src/main.c
+++ src/main.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h> // getenv
 
 int main(int argc, char **argv) {
-    printf("DEBUG: argc: %d, argv: %s\n", argc, argv[0]);
+    #ifndef _WIN32
+        printf("Not running on Windows. Exiting!\n");
+        return 1;
+    #endif
+
+    char* homePath = getenv("USERPROFILE");
+    
+    if (homePath) {
+        printf("Home directory: %s\n", homePath);
+    } else {
+        printf("Unable to retrieve home directory. Exiting!\n");
+        return 1;
+    }
 }
